38deacecf28f2c9778fdbf522e04c5d961d4b1dd
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-07-09 01:08:46 +0300

    Time: 110 ms (84.56%), Space: 15.5 MB (88.46%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,27 +1,38 @@
+from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        visit = [0 for _ in range(numCourses)]
-        red = set()
-        green = set()
-        for x, y in prerequisites:
-            graph[x].append(y)
-            
-        
-        def dfs(i):
-            if i in red:
-                return False
-            if i in green:
-                return True
-            red.add(i)
-            for j in graph[i]:
-                if not dfs(j):
-                    return False
-            red.remove(i)
-            green.add(i)
+        graph={}
+        indegree={}
+        v = numCourses
+        a = prerequisites
+        for i in range(v):
+            graph[i]=[]
+            indegree[i]=0
+        # {0: 0, 1: 0}
+        # print(indegree)
+        for edge in a:
+            parent=edge[0]
+            child=edge[1]
+            indegree[child]+=1
+            graph[parent].append(child)
+        # print(indegree) this is like incoming
+        # {0: 1, 1: 0}
+        # print(graph) childs
+        # {0: [1], 1: []} 
+        sources=deque()
+        for i,j in indegree.items():
+            if(j==0):
+                sources.append(i)
+        # print(sources)
+        # deque([0])
+        ans=[]
+        while sources:
+            c=sources.popleft()
+            ans.append(c)
+            for nbr in graph[c]: # graph[0] - [1] the nodes 
+                indegree[nbr]-=1 # indegree with number in 1 there was one incomming decrease
+                if(indegree[nbr]==0):
+                    sources.append(nbr)
+        if(len(ans)==v):
             return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+        return False
