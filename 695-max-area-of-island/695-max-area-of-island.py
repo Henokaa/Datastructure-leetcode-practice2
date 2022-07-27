5d61457a1569de563522bdbbb1b669fdfb078a00
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-07-27 11:43:14 +0300

    Time: 148 ms (88.97%), Space: 17.9 MB (9.21%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,45 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        parent = {}
-        rank = {}
-        direction = [[1,0], [0, 1], [-1, 0], [0, -1]]
-        inbound = lambda r, c: 0<= r <len(grid) and 0<= c <len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    parent[(i,j)] = (i, j)
-                    rank[(i,j)] = 1
-        
-        def findset(x):
-            if x == parent[x]:
-                return x
-            # parent[x] = findset(parent[x])
-            return findset(parent[x])
-        
-        def union(x,y):
-            x = findset(x)
-            y = findset(y)
+        visited = set()
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        row = len(grid)
+        cols = len(grid[0])
+        ans = []
+        self.temp = 1
+        value = 0
+        maxanswer = 0
+        def dfs(i,j):
+            visited.add((i,j))
+            for x,y in directions:
+                a = i + x
+                b = j + y
+                if 0 <= a < row and 0<= b < cols and grid[a][b] == 1 and (a,b) not in visited:
+                    self.temp += 1
+                    visited.add((a,b))
+                    dfs(a,b)
             
-            if x != y:
-                parent[x] = y
-                
+            
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    for r, c in direction:
-                        nr, nc = i + r, j + c
-                        if inbound(nr, nc) and grid[nr][nc] == 1:
-                            union((i,j), (nr,nc))
+            for j in range(len(grid[0])): # i keep forgetting
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    dfs(i,j)
+                    value +=1
+                    maxanswer = max(maxanswer, self.temp)
+                    self.temp = 1
         
-        count = {}
-        maxcount = 0
-        for i in parent.keys():
-            x = findset(i)
-            if x in count:
-                count[x] += 1
-            else:
-                count[x] = 1
-                
-            maxcount = max(maxcount, count[x])
-        return maxcount
-                
+        return maxanswer
