ad300af7af019c585640d108e6ad5764d152cf57
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-04 16:15:16 +0300

    Time: 83 ms (5.38%), Space: 14.2 MB (20.05%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,19 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Dynamic Programming
-        dp = {len(s): 1}
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
-
-            if i + 2 <= len(s) and (
-                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-            ):
-                dp[i] += dp[i + 2]
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return 1
+            cur = 0
+            if 1 <= int(s[i]) <= 9:
+                cur += dfs(i+1)
+            if 10 <= int(s[i:i+2]) <= 26 and len(s[i:i+2]) == 2:
+                cur += dfs(i+2)
+            return cur
+        return dfs(0)
+            
                 
-        print(dp)
-        return dp[0]
-    ''' TimeComplexity - o(n), space - o(n) but in this case you can use two variables and have the space down to o(1)'''
         
