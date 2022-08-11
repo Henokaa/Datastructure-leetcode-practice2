9d30d76c7b3705711b95d980a4e8aa3247a3183c
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-11 11:14:30 +0300

    Time: 3114 ms (17.84%), Space: 54.3 MB (5.08%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,18 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] *(amount + 1)
-        dp[0] = 0
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    if a - c == 0:
-                        dp[a] = 1  # +1 for that specific coin
-                    else:
-                        dp[a] = min(dp[a], dp[c] + dp[a - c]) 
-                        '''it first start as a dp[a] = inf but then with the first coin it changes so doesn't work without min b/c it takes the min for every coin ''' 
-                        # or 1 + dp[a - c]
-        # print(dp)
-        return dp[amount] if dp[amount] != float('inf') else -1
-    
-    ''' TimeComplexity: o(amount * len(coins))
-    space: o(n)'''
+        def dp(target):
+            if target in memo:
+                return memo[target]
+            if target == 0:
+                return 0
+            if target < 0:
+                return  target
+
+            shortest = float('inf')
+            for i in range(len(coins)):
+                if coins[i] <= target and target - coins[i] >= 0:
+                        shortest = min(shortest, dp(target - coins[i]) + 1)
+            memo[target] = shortest
+            return shortest
+        memo = {}
+        ans = dp(amount)
+        if ans == float('inf'):
+            return  -1
+        return ans
+        
