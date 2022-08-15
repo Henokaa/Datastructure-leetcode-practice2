ec7fa5e4dd6d1c794e20c43a170017cf31551478
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-16 02:11:33 +0300

    Time: 28 ms (97.92%), Space: 13.9 MB (66.18%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,16 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        a, b = 0, nums[0]
-        for i in range(1, len(nums)):
-            temp = max(a + nums[i], b)
-            a = b
-            b = temp
         
-        return b
+                # empty case
+        if not nums:
+            return 0
+        
+        dp = [None for x in range(len(nums)+1)]
+        # base case
+        dp[len(nums)] = 0
+        dp[len(nums)-1] = nums[len(nums)-1]
+        
+        for i in range(len(nums)-2, -1, -1):
+            dp[i] = max(dp[i+1], dp[i+2]+nums[i])
+        
+        return dp[0]
     
 #     1,2,3,1
 #     1,2,4,4
     
 #     2,1,1,2
 #     2,2,3,4
-# a=0, b=2, temp for the new one
