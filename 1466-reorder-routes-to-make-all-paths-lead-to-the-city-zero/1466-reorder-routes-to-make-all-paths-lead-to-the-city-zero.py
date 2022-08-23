8c01853a874dce4df63371a9b96e3f1384efcca2
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-23 16:03:03 +0300

    Time: 1311 ms (91.73%), Space: 54.8 MB (60.85%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,29 +1,27 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        from collections import defaultdict
         
-        undirected_graph = defaultdict(list)
-        actual_graph = defaultdict(set)  # for faster lookup
-
-        for a,b in connections:
-            undirected_graph[a].append(b) #Figure out which cities are connected to each other (i.e a road exists between the two) 
-            undirected_graph[b].append(a)
-            actual_graph[a].add(b)  #Figure out which cities have "valid" roads (i.e you are allowed to go from a to b)
-
-        queue = [(node, 0) for node in undirected_graph[0]] #All neighbours of 0 #(node, destination_node) 
+        # adjacency list and the actual list
+        graph = defaultdict(set)
+        adjacent = defaultdict(set)
+        
+        for x,y in connections:
+            graph[x].add(y)
+            adjacent[x].add(y)
+            adjacent[y].add(x)
+            
+        que = deque()
+        que.append((0, -1))
         res = 0
-        visited = set([0])# Mark 0 as visited
-        
-        while queue:
-            node, prev = queue.pop()  # destination node parent or prev node
-            visited.add(node) #Mark node as visited 
-
-            if not (prev in actual_graph[node]):   #If there is no "valid" road which lets you travel from node to destination_node, you'll have to reverse the road
-                res += 1
-
-            for neighb in undirected_graph[node]: #For all unvisited neighbours, you'll have to continue exploring
-                if neighb not in visited:
-                    queue.append((neighb, node))
-        
+        while que:
+            var, prev = que.pop()
+            for child in adjacent[var]:
+                if child == prev:
+                    continue
+                if var not in graph[child]:
+                    res += 1
+                que.append((child, var))
+                
         return res
-        
+                    
+                    
