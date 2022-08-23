a3077c5ad69bc59d3a1d160b1868c81acf679a10
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-23 21:56:55 +0300

    Time: 143 ms (6.46%), Space: 14.4 MB (85.98%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+
+        l, r = 0, len(s) - 1
+        while l <= r:
+            while l < r and not self.alphanum(s[l]):  # not equal to <= because it will go beyond the equal and check then exit. that will create out of index 
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
     # the interviewer may not like it because we extra memory for the string and reversing takes extra space
