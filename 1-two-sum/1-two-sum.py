8715c91fc3c2534cf32957857ebf9bb379d3deb0
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-27 00:37:08 +0300

    Time: 76 ms (83.78%), Space: 15.2 MB (24.20%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, n in enumerate(nums):
-            if target - n in nums and i != nums.index(target - n):
-                return i, nums.index(target - n)
+        # two pass o(n)
+        checker = {} # it couldn't set because there is duplicates in the array and they can be an answer
+        for i in range(len(nums)):
+            checker[nums[i]] = i
+        
+        for i in range(len(nums)):
+            res = target - nums[i] 
+            if res in checker and checker[res] != i:
+                return [i, checker[res]]
+        return []
+                        
+                
