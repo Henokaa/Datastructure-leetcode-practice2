ebb630fd24c7a3fe634536cf497aa70b30ed0cff
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-02 01:22:41 +0300

    Time: 83 ms (14.56%), Space: 14 MB (70.62%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,20 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        def dp(cur):
-            if cur in memo:
-                return memo[cur]
-            if cur == s:
-                return True
-            
-            
-            for word in wordDict:
-                if cur+word != s[:len(cur+word)]:
-                    continue
-                if dp(cur + word):
-                    return True
-            
-            memo[cur] = False
-            return False
-            
-        memo = {}
-        return dp("")
+        
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i:i+len(w)] == w:
+                    dp[i]=dp[i+len(w)] # because there is a repetition in char its like + 1
+
+                if dp[i]: # if one become successful end
+                    break
+        return dp[0]
+                
+        # time complexity - 0(n*m*n) the last n for comparing
+        
+        
