73915a99c00a4f9ffb4b25a7a5cb40be80f77d54
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-05 22:20:59 +0300

    Time: 203 ms (52.08%), Space: 15 MB (10.58%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        checker = {}
+        r = 0
+        while r < len(numbers):
+            temp = target - numbers[r]
+            if  temp in checker:
+                return [checker[temp]+1, r+1]
+            else:
+                checker[numbers[r]] = r
+                r += 1
+        
+'''
+[2,7,11,15]
+'''
