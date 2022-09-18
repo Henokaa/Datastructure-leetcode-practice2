eb4f6edcd5a74bc935308a2e8593e7ecf1caebd0
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-19 01:16:18 +0300

    Time: 237 ms (17.46%), Space: 18 MB (82.79%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,22 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        ''' Kth largest o((n-k)(logn)) at most o(nlogn)
+        for the add time complexity - its is just logn
+        if it was an array because we have to move everything o(n)'''
+        # minHeap w/ K largest integers
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
