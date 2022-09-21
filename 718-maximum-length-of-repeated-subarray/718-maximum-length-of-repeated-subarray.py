f8eab36b6e48b17498d68d34713b9e8419bd0025
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-21 20:38:13 +0300

    Time: 5202 ms (60.74%), Space: 39.2 MB (74.08%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,27 +1,14 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        ''' 
-        N1 = [1,2,3,2,1]
-              ^
-        N2 = [3,2,1,4,7]
-                ^
-        This is offset technique, starting from both array
-        Time - 0(n^2)
-        space - o(1)
         '''
-        def findL(nums1, nums2):
-            N1 = len(nums1)
-            N2 = len(nums2)
-            best = 0
-            for j in range(N2):
-                counter = 0
-                for i in range(N1):
-                    if j + i >= N2:
-                        break
-                    if nums1[i] == nums2[j + i]:
-                        counter += 1
-                        best = max(best, counter)
-                    else:
-                        counter = 0
-            return best
-        return max(findL(nums1, nums2), findL(nums2, nums1))
+        '''
+        dp = [[0] * (len(nums2)+1) for i in range(len(nums1) + 1 )]
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+        
+        return max(max(row) for row in dp)
+            
