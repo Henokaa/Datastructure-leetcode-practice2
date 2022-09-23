f37cc145f8f92c56e3c6a8ce74660d8ec0d0107d
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-23 11:44:00 +0300

    Time: 762 ms (5.01%), Space: 21.5 MB (40.76%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,33 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         '''
-        [1,2,3,4]
-        [24 , 24, 12,4]
-        [1  ,  2, 6, 24]
+        [1, 2, 3, 4]
+        [1, 1, 2, 6]
+        [24, 12, 8, 6]
+        
+  pre =[1,2, 6, 24]
+      =[1, 1, 2, 6]
+  post=[24 , 24, 12,4]
+        
         '''
-        preFix = []
-        preSum = 1
-        for i in range(len(nums)):
-            preSum *= nums[i]
-            preFix.append(preSum)
-    
         
-        postFix = []
-        postSum = 1
-        for i in range(len(nums) - 1, -1, -1):
-            postSum *= nums[i]
-            postFix.append(postSum)
-        
-        postFix = postFix[::-1]
-        ans = []
-        print(postFix)
-        temp = 1
-        for i in range(len(nums)):
-            if i - 1 >= 0:
-                temp *= preFix[i-1]
-            if i+1 < len(nums):
-                temp *= postFix[i + 1]
-            ans.append(temp)
-            temp = 1
-        
-        return ans
+        res = [1] * len(nums)
+        for i in range(1, len(nums)): # from left to right 
+            res[i] = res[i-1] * nums[i-1]
+        print(res)
+        tmp = 1
+        for i in range(len(nums)-2, -1, -1): # from right to left
+            tmp *= nums[i+1]
+            res[i] *= tmp
+        print(res)
+        return res
