96ecc052493bb538bb17f3fabada7f9e879a5429
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-09-23 11:03:59 +0300

    Time: 282 ms (79.16%), Space: 22.2 MB (28.35%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,23 +1,33 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer = [1] * len(nums)
-        
-        res = 1
+        '''
+        [1,2,3,4]
+        [24 , 24, 12,4]
+        [1  ,  2, 6, 24]
+        '''
+        preFix = []
+        preSum = 1
         for i in range(len(nums)):
-            if i == 0:
-                res *= nums[i] 
-                continue
-            answer[i] = res
-            res *= nums[i]
+            preSum *= nums[i]
+            preFix.append(preSum)
+    
         
-        res = 1
-        for j in range(len(nums)-1 , -1, -1):
-            if j == len(nums) - 1:
-                res *= nums[j]
-                continue
-            
-            answer[j] *= res
-            res *= nums[j]
+        postFix = []
+        postSum = 1
+        for i in range(len(nums) - 1, -1, -1):
+            postSum *= nums[i]
+            postFix.append(postSum)
         
-        return answer
-            
+        postFix = postFix[::-1]
+        ans = []
+        print(postFix)
+        temp = 1
+        for i in range(len(nums)):
+            if i - 1 >= 0:
+                temp *= preFix[i-1]
+            if i+1 < len(nums):
+                temp *= postFix[i + 1]
+            ans.append(temp)
+            temp = 1
+        
+        return ans
