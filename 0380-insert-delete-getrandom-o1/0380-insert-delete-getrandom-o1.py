0ab71d4e4794674985a52a465f2eec4542044c98
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-21 16:47:22 +0300

    Time: 1105 ms (35.48%), Space: 61.4 MB (54.71%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -17,22 +17,22 @@ class RandomizedSet:
     def remove(self, val: int) -> bool:
         if val not in self.dictionary:
             return False
-        if self.array[-1] == val:
-            del self.dictionary[val]
-            self.array.pop()
-            return True
+        # if self.array[-1] == val:
+        #     del self.dictionary[val]
+        #     self.array.pop()
+        #     return True
         temp = self.dictionary[val]
             
-        del self.dictionary[val]
-        last = len(self.array) - 1
         
+        last = len(self.array) - 1
+        # del self.dictionary[val]          not here
         self.dictionary[self.array[last]] = temp 
         
         swap = self.array[temp]
         self.array[temp] = self.array[last]
         self.array[last] = swap
         self.array.pop()
-        
+        del self.dictionary[val]   # this should be here
         # print(self.array, temp)
         return True
 
