a9c666171cad471eee2c74f85773bad8a81d3c4d
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-22 18:15:27 +0300

    Time: 62 ms (39.99%), Space: 13.9 MB (19.92%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -6,34 +6,20 @@ class Solution:
                ^
         '''
         #dictionary for the 7 elements
-        words = defaultdict(int)
-        for i in range(k):
-            if blocks[i] in words:
-                words[blocks[i]] += 1
-            else:
-                words[blocks[i]] = 1
-        # print(words)
-        
-        # sliding window
-        l = 0
-        r = k - 1
-        result = float('inf')
-        flag = 0
-        if r == l:
-            flag = 1
-        while r <= len(blocks) - 1:
-            operations = words['W']
-            result = min(result, operations)
-            if r == len(blocks) - 1:
-                break
-            if flag == 1:
-                words[blocks[r]] -= 1
-                r += 1
-                words[blocks[r]] += 1
-                continue
-            words[blocks[l]] -= 1
-            l += 1
-            r += 1
-            words[blocks[r]] += 1
+        best = 10 ** 20
+        current = 0
+        '''
+        "WBBWWBBWBW"
+                ^
+        '''
+        N = len(blocks)
+        for i in range(N):
+            # print(blocks[i], current)
+            if blocks[i] == "W":
+                current += 1
+            if i - k >= 0 and blocks[i-k] == "W":
+                current -= 1
             
-        return result
+            if i - k + 1 >= 0:
+                best = min(best, current)
+        return best
