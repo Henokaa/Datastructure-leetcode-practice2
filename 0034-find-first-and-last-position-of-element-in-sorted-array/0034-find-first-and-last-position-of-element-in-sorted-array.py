e77ec034e14b96c00750907c694b2d41a89b4cc8
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-22 11:08:45 +0300

    Time: 96 ms (86.17%), Space: 15.5 MB (49.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,35 +7,46 @@ class Solution:
         if mid > target:
             mif = r - 1
         '''
-        l = 0
-        r = len(nums) - 1
-        start = float('inf')
-        end = float('-inf')
-        while l <= r:
-            mid = l + (r - l)//2
-            if nums[mid] > target:
-                r = mid - 1
-            elif nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] == target:
-                start = min(start, mid)
-                r = mid - 1
+        def binarymin(nums, target):
+            left = 0
+            right = len(nums) - 1
+            start = float('inf')
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] > target:
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] == target:
+                    start = mid
+                    right = mid - 1
+            if start == float('inf'):
+                return -1
+            else:
+                return start
+        def binarymax(nums, target):
+            left = 0
+            right = len(nums) - 1
+            end = float('INF')
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] > target:
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] == target:
+                    end = mid
+                    left = mid + 1
+            if end == float('inf'):
+                return -1
+            else:
+                return end
         
-        l = 0
-        r = len(nums) - 1
-        while l <= r:
-            mid = l + (r - l)//2
-            if nums[mid] > target:
-                r = mid - 1
-            elif nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] == target:
-                end = max(end, mid)
-                l = mid + 1
-                
-        if start == float('inf'):
-            return [-1, -1]
-        else:
-            return [start, end]
+        begin = binarymin(nums, target)
+        end = binarymax(nums, target)
+        return [begin, end]
             
+        
+        
+        
             
