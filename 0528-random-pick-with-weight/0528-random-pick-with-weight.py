a831e4da4417eece614944202618715006df48e5
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-24 17:50:18 +0300

    Time: 589 ms (38.91%), Space: 18.7 MB (23.84%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,39 +1,27 @@
-import random
 class Solution:
+
     def __init__(self, w: List[int]):
-        '''
-        [1, 4, 6]
-        r = 2
-        if not r <= nums[mid]:
-            l = mid + 1
-    
-        '''
-        self.w = []
+        self.prefix = []
         total = 0
-        for weight in w:
-            total += weight
-            self.w.append(total)
-        self.total = total
-        
-        
+        for num in w:
+            total += num
+            self.prefix.append(total)
+            
+
     def pickIndex(self) -> int:
-        ran= random.randint(1, self.total)
-        '''
-        [1, 3]
-        [1, 4, 6]
+        random_num = random.randint(1, self.prefix[-1])
         
-        '''
-        answer = float('inf')
-        left = 0
-        right = len(self.w) - 1
+        left, right = 0, len(self.prefix) - 1
+        best = -1
         while left <= right:
-            mid = left + (right - left) //2
-            if ran <= self.w[mid]:
-                answer = mid 
+            mid = (left + right) // 2
+            if self.prefix[mid] >= random_num:
+                best = mid
                 right = mid - 1
-            elif ran > self.w[mid]:
+            else:
                 left = mid + 1
-        return answer
+        return best
+
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
