f2e05a6360b455b2710309d512c0fca1174c578d
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-25 14:42:23 +0300

    Time: 1872 ms (80.43%), Space: 28.3 MB (36.65%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,29 +1,40 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         '''
-        
-        [1,4,8,13]     k = 5
-             
-        sort
-        num[r] * len - total - will give as how many k we need and compare it with the k that we         have
-        
-        we are to make everything num[r] 
-        and num[r] * len = the prefix sum that we need
-        total - the prefix sum that we have
-
+        [1,4,8,13]  k = 5
+         ^
+           ^
+         24, 13
+         [1,5,13,26]
         '''
+        # prefix
         nums = sorted(nums)
-        r = 0
-        l = 0
-        mx = float('-inf')
         total = 0
-        while r < len(nums):
-            total += nums[r]
-            # print(nums[r] * (r - l + 1) - total)
-            while nums[r] * (r - l + 1) - total > k:
-                total -= nums[l]
-                l += 1
-            mx = max(mx, r - l + 1)
-            r += 1
+        pre = []
+        for i in nums:
+            total += i
+            pre.append(total)
         
-        return mx
+        # making our sliding window, return the max valid window length
+        r = 1
+        l = 0
+        answer = 1
+        while r < len(nums):
+            window = r - l + 1
+            needed = window*nums[r]
+            if l - 1 >= 0:
+                have = pre[r] - pre[l - 1]
+            else:
+                have = pre[r]
+            total = needed - have
+            if total <= k:
+            
+                answer = max(answer, window)
+                r += 1
+            elif total > k:
+                l += 1
+        return answer
+            
+            
+            
+        
