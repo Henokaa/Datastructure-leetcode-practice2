543ae9a95ec998be3f0c3b0c83db3709c471b619
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-25 15:51:42 +0300

    Time: 1249 ms (30.33%), Space: 23.9 MB (20.38%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.py
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -1,24 +1,33 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        '''
+        
+        '''
+        # Do the graph
         graph = defaultdict(list)
-        indegree = Counter()
-        queue = deque([])
-        answer = [node for node in range(len(quiet))]
-        for a, b in richer:
-            graph[a].append(b)
-            indegree[b] += 1
-        print(graph)
-        for node in range(len(quiet)):
-            if indegree[node] == 0:
-                queue.append(node)
-        while queue:
-            print(queue)
-            front = queue.popleft()
-            for child in graph[front]:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    queue.append(child)
-                if quiet[answer[front]] < quiet[answer[child]]:
-                    answer[child] = answer[front]
+        for x, y in richer:
+            graph[y].append(x)
+        memo = {}
+        # dfs through the graph
+        def dfs(i):
+            if (quiet[i],i) in memo:
+                return memo[(quiet[i],i)]
+            
+            mini = (float('inf'),-1) 
+            for child in graph[i]:
+                temp1 = dfs(child)
+                mini = min(mini, (temp1[0], temp1[1]))
+                
+            mini = min(mini, (quiet[i], i))
+            memo[(quiet[i],i)] = mini
+            return mini
+        
+        answer = []
+        for i in range(len(quiet)):
+            # temp2 = dfs(i)
+            answer.append(dfs(i)[1])
+            
         return answer
+       
+        
         
