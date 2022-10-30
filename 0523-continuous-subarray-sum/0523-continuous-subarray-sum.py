f1d987ec91154b492e4e0c9876d2b994a27943b6
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-10-30 23:31:07 +0300

    Time: 3449 ms (5.00%), Space: 31.4 MB (89.17%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,23 +1,41 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         '''
-        To understand this question, you have to understand how remainders work. If k = 5, then 6 % 5 = 1, which also means (6 + 5) % 5 = 1. So you use a hash map to check if you have seen that remainder before. If yes, then you know between both indexes, it is 5 (i.e. (6 + 5) - 6 = 5). This question tests nothing other than knowing how remainders work.
-        eg - 23 % 6 = 5
-        (23 + 2 + 4)% 5 = 5 so 2 + 4 is multiple of 5
+        [23,2,4,6,7]
+        
+        [23,25,29,35,42]
+        [5, 1, 5, 5, 0]
+        
+        [23,2,6,4,7]
+        [23,25,31,35,42]
+        
         '''
-        
-        remainder = { 0: -1 }
+        prefix = []
+        save = defaultdict(int)
+        r = 0
         total = 0
-        
-        for i, n in enumerate(nums):
-            total += n
-            r = total % k
-            if r not in remainder:
-                remainder[r] = i
+        ans = 0
+        while r < len(nums):
+            total += nums[r]
+            prefix.append(total)
+            curr = total % k
+            if len(prefix) >= 3:
+                value = prefix[r-2]
+                save[value%k] += 1
                 
-            elif i - remainder[r] > 1:
+            
+            if curr in save:
+                ans += save[curr]
                 return True
+                
+            if r >= 1 and curr == 0:
+                ans += 1
+                return True
+            
+            
+            r += 1
         return False
-        
+     
+        ''' do it with constant space'''
         
         
