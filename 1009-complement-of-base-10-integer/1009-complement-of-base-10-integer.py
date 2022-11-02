dd2fde1af2d07fc3793a9760d1a1990b9e9f57b3
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-02 11:36:21 +0300

    Time: 28 ms (97.35%), Space: 14 MB (8.96%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -1,13 +1,14 @@
 class Solution:
     def bitwiseComplement(self, num: int) -> int:
         ans = 0
-        n = 0
-        for i in range(31, -1, -1):
-            if num & 1 << i:
-                n = i
-                break
-        for i in range(n + 1):
-            res = 1 - (num >> i & 1)
-            ans = ans | res << i
+        base = 0
+        i = 0
+        while base <= num and i < 32:
+            print((num >> i & 1))
+            flipped = 1 - (num >> i & 1)
+            ans = ans | flipped << i
+            base = (1 << (i + 1))
+            i += 1
 
+       
         return ans
