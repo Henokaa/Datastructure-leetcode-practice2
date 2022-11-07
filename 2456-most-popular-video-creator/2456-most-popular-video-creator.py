5c2d1b487e4ab0d4b185027d79e757cfb0a5af7d
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-07 12:57:18 +0300

    Time: 816 ms (99.52%), Space: 60.5 MB (80.63%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.py
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.py
@@ -1,49 +1,30 @@
 class Solution:
     def mostPopularCreator(self, creators: List[str], ids: List[str], views: List[int]) -> List[List[str]]:
         '''
-        alice : [0,2
-        
-        ids: [
-        
-        one: 5
+        alice : [(one,5) 
         
         '''
-        name = defaultdict(list)
-        vw = defaultdict(int)
-        for i in range(len(views)):
-            if creators[i] in name:
-                for elements in name[creators[i]]:
-                    if views[i] > views[elements]:
-                        name[creators[i]] = []
-                        name[creators[i]].append(i)
-                        break
-                    elif views[i] == views[elements]:
-                        name[creators[i]].append(i)
-                        break
-                    else:
-                        break
-            else:
-                name[creators[i]].append(i) 
-                
-        # print(name)
-        
-        
+        total = defaultdict(int)
+        saved = {}
         mx = float('-inf')
-        for i in range(len(views)):
-            vw[creators[i]] += views[i]
-            mx = max(mx, vw[creators[i]])
-        # print(mx)
-        
-        
+        for creator, i, view in zip(creators, ids, views):
+            total[creator] += view
+            if creator not in saved:
+                saved[creator] = (i, view)
+            else:
+                if view > saved[creator][1]:
+                    saved[creator] = (i, view)
+                elif view == saved[creator][1] and i < saved[creator][0]:
+                    saved[creator] = (i, view)
+                    
+        mx = max(total.values())
         ans = []
-        temp = []
-        for x,y in vw.items():
-            if y == mx:
-                for ele in name[x]:
-                    temp.append(ids[ele])
-                names = sorted(temp)
-                ans.append([x, names[0]])
-                temp = []
-            
+        for x in total.keys():
+            if total[x] == mx:
+                ans.append([x,saved[x][0]])
+                
         return ans
         
+            
+                    
+            
