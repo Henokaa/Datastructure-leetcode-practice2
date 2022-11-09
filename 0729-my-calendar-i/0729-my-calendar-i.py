acc7810ad21a7edff98848b9fb4a783a095826c6
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-09 21:35:53 +0300

    Time: 657 ms (52.74%), Space: 15.3 MB (5.89%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,13 +1,26 @@
 from sortedcontainers import SortedList
 class MyCalendar:
     def __init__(self):
-        self.calendar = []
+        self.s1 = SortedList()
+        
+        
 
-    def book(self, start, end):
-        for s, e in self.calendar:
-            if s < end and start < e:
+    def book(self, start: int, end: int) -> bool:
+        end -= 1
+        index = self.s1.bisect_left((start, -1))
+
+        
+        if index < len(self.s1):
+            left, right = self.s1[index]
+            if left <= start <= right or left <= end <= right or start <= left <= end or start <= right <= end:
                 return False
-        self.calendar.append((start, end))
+            
+        if index - 1 >= 0:
+            left, right = self.s1[index - 1]
+            if left <= start <= right or left <= end <= right or start <= left <= end or start <= right <= end:
+                return False
+            
+        self.s1.add((start, end))
         return True
 
 # Your MyCalendar object will be instantiated and called as such:
