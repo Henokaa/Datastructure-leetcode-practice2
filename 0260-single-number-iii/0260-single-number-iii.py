74b4d280837a990b0652051c508a1cebc2f5e395
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-10 15:29:15 +0300

    Time: 103 ms (78.47%), Space: 16.2 MB (16.54%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,18 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        ans=0
-        for item in nums:
-            ans=ans^item
-        t=ans
-        count=0
-        while(t):
-            if t&1==1:
-                break
-            count+=1
-            t=t>>1
-        b=ans
-        for item in nums:
-            if (item>>count)&1==1:
-                b=b^item
-        return [b,ans^b]
-        
+        freq = Counter(nums)
+        return [x for x in freq if freq[x] == 1]
