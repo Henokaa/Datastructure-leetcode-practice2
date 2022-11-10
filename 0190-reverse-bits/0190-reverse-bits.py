d4e7d24f4312619ab1399e1b2c04c42bee1c8d35
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-10 14:21:04 +0300

    Time: 62 ms (50.97%), Space: 13.9 MB (8.10%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        ans = ""
         res = 0
-        print(1 << 2)
         for i in range(32):
-            bit = (n >> i) & (1)
-            # bit = (n) & (1 << i) this will give you numbers like 4 when its in sec pos
-            res |= (bit << (31 - i))
+            ans = n & 1
+            res |= ans << (31 - i)
+            n = n >> 1
+    
         return res
