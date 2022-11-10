71ae46568f8f8ca4569b3cc679067942eabcc256
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-10 22:56:05 +0300

    Time: 54 ms (64.90%), Space: 13.9 MB (73.97%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -1,22 +1,9 @@
 class Solution:
-    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+    def haveConflict(self, e1: List[str], e2: List[str]) -> bool:
         '''
         
         '''
-        if event1[0] <= event2[0]:
-            e1 = event1
-            e2 = event2
-        else:
-            e2 = event1
-            e1 = event2
-            
-        if e1[1] < e2[0]:
-            return False
-        else:
-            if e1[0] > e2[0]:
-                return False
-            else:
-                return True 
+        return e1[0] <= e2[1] and e1[1] >= e2[0]
             
         
         
