a4e3305087318a0bce148247a7afa450ef44c0ff
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-14 14:42:37 +0300

    Time: 5874 ms (61.30%), Space: 48.8 MB (18.36%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,37 +1,43 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
+        '''
+        visited((nr,nc)) = k is a must otherwise TLE, i think optimization technqiue 
+        time-complexity - E*logE
+        space-complexity - 0(E)
+        '''
         # que bfs
-        R = len(grid)
-        C = len(grid[0])
-        
-        INF = 10 ** 20
-        
-        h = []
-        heapq.heappush(h, (0,0,0))
-        distances = {}
-        distances[(0,0)] = 0
-        directions = [(1,0), (0, 1), (-1, 0), (0, -1)]
-        while len(h) > 0:
-            d,x,y = heapq.heappop(h)
+        que = []
+        visited = {}
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        inbound = lambda r,c: 0<=r<len(grid) and 0<=c<len(grid[0])
+        k = 0
+        if grid[0][0] == 1:
+            k += 1
+        heapq.heappush(que, (k,0,0))
+        visited[(0,0)] = k
+        while que:
+            k, x, y = heapq.heappop(que)
             
-            if d > distances[(x,y)]:
+            if visited[(x,y)] < k:
                 continue
-            if x == R-1 and y == C-1:
-                return d
-            
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
                 
-                if 0<= nx < R and 0 <= ny < C:
-                    nd = d
-                    
-                    if grid[nx][ny] == 1:
-                        nd += 1
-                    if (nx,ny) not in distances or distances[(nx,ny)] > nd:
-                        distances[(nx,ny)] = nd
-                        heapq.heappush(h, (nd, nx, ny))
-                        
-#         return distances[R - 1][C - 1]
+            if x == len(grid)-1 and y == len(grid[0])-1:
+                return k
+            
+            
+                
+            for r, c in directions:
+                nr = x + r
+                nc = y + c
+                
+                if inbound(nr,nc):
+                    nk = k
+                    if grid[nr][nc] == 1:
+                        nk += 1
+                    if (nr,nc) not in visited or visited[(nr,nc)] > nk:
+                        heapq.heappush(que, (nk,nr,nc))
+                        visited[(nr,nc)] = nk
+                
         
                 
         
