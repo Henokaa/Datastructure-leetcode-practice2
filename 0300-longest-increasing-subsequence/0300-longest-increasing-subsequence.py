0e104ba3a3e034d6bc872d7aa6f44acf15c9a20c
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-18 13:53:19 +0300

    Time: 235 ms (71.42%), Space: 14.3 MB (12.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -7,15 +7,14 @@ class Solution:
                 mid = lo + (hi - lo)//2
                 if sub[mid] < val:
                     lo = mid + 1
-                elif val < sub[mid]:
+                elif val <= sub[mid]:
                     hi = mid - 1
-                else:
-                    return mid
+                
             return lo
         
         sub = []
         for val in nums:
-            pos = bisect_left(sub, val)
+            pos = binarySearch(sub, val)
             # because for rg 3 its one
             if pos == len(sub):
                 sub.append(val)
