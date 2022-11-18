985f1ca0d286aed2585fffeaee1a120bed357f12
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-18 13:49:55 +0300

    Time: 70 ms (98.72%), Space: 14.3 MB (12.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,29 +1,30 @@
+from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        '''
-        https://www.reddit.com/r/leetcode/comments/y7igv5/tanked_one_of_the_faang_interview_feel_miserable/
-        '''  
-        '''Bruteforce - for every number either take it or don't so 2 option for every n. 2^n
-        dp - time complexity - o(n^2)'''
+        def binarySearch(sub, val):
+            lo, hi = 0, len(sub)-1
+            while(lo <= hi):
+                mid = lo + (hi - lo)//2
+                if sub[mid] < val:
+                    lo = mid + 1
+                elif val < sub[mid]:
+                    hi = mid - 1
+                else:
+                    return mid
+            return lo
         
-        '''
-        this time complexity is - i * prev = nums.length * nums[i]
-        '''
-        length = len(nums)
-        dp = [0] * len(nums)
+        sub = []
+        for val in nums:
+            pos = bisect_left(sub, val)
+            # because for rg 3 its one
+            if pos == len(sub):
+                sub.append(val)
+            else:
+                sub[pos] = val
+            
+            # print(val , pos, sub)
+        return len(sub)
         
-        dp[length-1] = 1
-        ans = 1
-        for i in range(len(nums)-2, -1, -1):
-            temp = 1
-            for j in range(i + 1, length):
-                # print(i,j)
-                if nums[i] < nums[j]:
-                    temp = max(temp, 1 + dp[j])
-            dp[i]= temp 
-            ans = max(temp, ans)
-        # print(dp)
-        return ans
                 
             
                 
