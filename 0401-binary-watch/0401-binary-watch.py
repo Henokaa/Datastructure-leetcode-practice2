578ac334078f8f8dd95636df36caf81f288bce3c
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-11-18 10:13:20 +0300

    Time: 73 ms (22.12%), Space: 14 MB (29.19%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,15 +1,26 @@
-class Solution:
-    def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        '''
-        1 2 4 8
+class Solution(object):    
+    def readBinaryWatch(self, num):
+        def dfs(LEDS, idx, hrs, mins, n):
+            
+            
+            if hrs >= 12 or mins >= 60:
+                return
+            if n == 0:
+                time = str(hrs) + ":" + "0"*(mins<10) + str(mins)
+                result.append(time)
+                return
+
+            if idx < len(LEDS):
+                if idx <= 3:  
+                    dfs(LEDS, idx+1, hrs + LEDS[idx], mins, n-1)
+                else:  
+                    dfs(LEDS, idx+1, hrs, mins + LEDS[idx], n-1)
+                dfs(LEDS, idx+1, hrs, mins, n)
+        result = []
+        LEDS = [
+            8, 4, 2, 1,  
+            32, 16, 8, 4, 2, 1 
+        ]
+        dfs(LEDS, 0, 0, 0, num)
         
-        1 2 4 8 16 32
-        '''
-        output = []
-        for h in range(12):
-            for m in range(60):
-                temp = bin(h) + bin(m)
-                if temp.count("1") == turnedOn:
-                    output.append((str(h)+":"+str(m).zfill(2)))
-                    
-        return output
+        return result
