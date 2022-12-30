4082c0d51e8c48b5a812ee936561a90021e34389
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-12-30 07:44:36 +0300

    Time: 104 ms (84.93%), Space: 16 MB (12.77%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,20 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
-        
-        ans = []
-        def dfs(i, path):
-            if i == len(graph) - 1:
-                ans.append(path.copy())
-                return
-            
-            
-            for child in graph[i]:
-                path.append(child)
-                dfs(child, path)
-                path.pop()
-                
-        
-        dfs(0, [0])
-                
-        return ans
+        result = []
+        dq = deque([(0, [0])])
+        target = len(graph) - 1
+        while dq:
+            # print(dq)
+            cur,route = dq.popleft()
+            if cur == target:
+                result.append(route)
+            else:
+                for node in graph[cur]:
+                    route.append(node)
+                    dq.append((node, route.copy()))
+                    route.pop()
+        return result
