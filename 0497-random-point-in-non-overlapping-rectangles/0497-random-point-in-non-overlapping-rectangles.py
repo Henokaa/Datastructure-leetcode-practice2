c9c50b374c3a3d75808c7975efc32114779d7d39
Henok F <69524541+Henokaa@users.noreply.github.com>
2023-01-03 10:12:44 +0300

    Time: 566 ms (18.37%), Space: 18.6 MB (40.82%) - LeetHub

--- 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.py
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.py
@@ -1,29 +1,28 @@
 class Solution:
 
     def __init__(self, rects: List[List[int]]):
+        w = [(x2-x1+1)*(y2-y1+1) for x1,y1,x2,y2 in rects]
+        print(w)
         total = 0
-        self.rects = []
+        self.weights = []
+        for i in w:
+            total += i
+            self.weights.append(total)
+        print(self.weights)
+        self.rects = rects
+
         
-        for x1, y1, x2, y2 in rects:
-            area = (x2 - x1 + 1) * (y2 - y1 + 1)
-            total += area
-        
-        self.total = total
-        
-        running_weight = 0
-        for x1, y1, x2, y2 in rects:
-            weight = ((x2-x1+1) * (y2 - y1 + 1)) / total
-            running_weight += weight
-            self.rects.append((running_weight, x1, y1, x2, y2))
+            
         
     def pick(self) -> List[int]:
-        r = random.random()
-        for weight, x1, y1, x2, y2 in self.rects:
-            if weight > r:
-                x = random.randint(x1,x2)
-                y = random.randint(y1,y2)
-                
-                return [x,y]
+        rand = random.randint(1, self.weights[-1] - 1)
+        n_rect = bisect.bisect(self.weights, rand)
+        # print(rand, n_rect ,self.weights[n_rect])
+        # print(n_rect, rand)
+        x1, y1, x2, y2 = self.rects[n_rect] 
+        return [random.randint(x1, x2),random.randint(y1, y2)]
+        
+        
                 
         
 
