f7ea2d3f36b0094ad7c5d2b52f2399cfad39859d
Henok F <69524541+Henokaa@users.noreply.github.com>
2023-02-14 21:09:13 +0300

    Time: 43 ms (50.53%), Space: 13.8 MB (55.87%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        occurrence = Counter(str(n))
+        for i in range(30):
+            if (occurrence == Counter(str(2**i))):
+                return True
+        return False
+        
