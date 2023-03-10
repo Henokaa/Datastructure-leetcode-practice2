a4cf74b7e627864b6d1512755d331d9bd14e06eb
Henok F <69524541+Henokaa@users.noreply.github.com>
2023-03-10 15:25:55 +0300

    Time: 257 ms (70.63%), Space: 15.2 MB (12.97%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,34 +1,28 @@
+from sortedcontainers import SortedList
+
 class MyCalendar:
     def __init__(self):
-        self.bookings = []
+        self.bookings = SortedList()
     
     def book(self, start: int, end: int) -> bool:
-        # find the correct position to insert the new booking using binary search
-        left, right = 0, len(self.bookings)
-        while left < right:
-            mid = (left + right) // 2
-            if self.bookings[mid][1] <= start:
-                left = mid + 1
-            elif self.bookings[mid][0] >= end:
-                right = mid
-            else:
-                return False
+        # check if there is any overlap with an existing booking
+        i = self.bookings.bisect_right((start,end))
+        if i > 0 and self.bookings[i-1][1] > start:
+            return False
+        if i < len(self.bookings) and end > self.bookings[i][0]:
+            return False
         
         # insert the new booking at the correct position
-        self.bookings.insert(left, (start, end))
+        self.bookings.add((start, end))
         return True
+
         
 
 # brute force
 '''
-def book(events, new_event) -> bool:
-        for event in events:
-            if new_event[0] < event[1] and new_event[1] > event[0]:
-                # new event overlaps with existing event
-                return False
-        # no overlap found, can add the new event
-        return True
+In this implementation, we use a SortedList object to store the bookings. The SortedList automatically keeps the elements in sorted order, so we don't need to call the .sort() method as we did in the previous implementations.
+
+To check if there is any overlap with an existing booking, we use the bisect_right method of the SortedList object to find the correct position to insert the new booking. If there is any overlap with the previous or next booking, we return False.
+
+The time complexity of this approach is O(nlogn) for each booking because the bisect_right method has a time complexity of O(logn), and we call it twice for each booking. The space complexity is O(n) to store the bookings in a SortedList.
 '''
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
