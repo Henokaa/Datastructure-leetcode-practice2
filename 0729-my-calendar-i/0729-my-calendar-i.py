562c34d21949dd0baa88848a940a825d7a147d18
Henok F <69524541+Henokaa@users.noreply.github.com>
2023-03-10 12:23:59 +0300

    Time: 205 ms (95.66%), Space: 14.6 MB (90.19%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,20 +1,34 @@
-from sortedcontainers import SortedList
-'''
-[10, 20]
-
-'''
 class MyCalendar:
     def __init__(self):
-        self.calendar = SortedList()
-
+        self.bookings = []
+    
     def book(self, start: int, end: int) -> bool:
-        idx = self.calendar.bisect_left((start, end))
-        # print(self.calendar, idx, (start, end))
-        if (idx > 0 and self.calendar[idx-1][1] > start) or (idx < len(self.calendar) and self.calendar[idx][0] < end):
-            return False
-        self.calendar.add((start, end))
+        # find the correct position to insert the new booking using binary search
+        left, right = 0, len(self.bookings)
+        while left < right:
+            mid = (left + right) // 2
+            if self.bookings[mid][1] <= start:
+                left = mid + 1
+            elif self.bookings[mid][0] >= end:
+                right = mid
+            else:
+                return False
+        
+        # insert the new booking at the correct position
+        self.bookings.insert(left, (start, end))
         return True
+        
 
+# brute force
+'''
+def book(events, new_event) -> bool:
+        for event in events:
+            if new_event[0] < event[1] and new_event[1] > event[0]:
+                # new event overlaps with existing event
+                return False
+        # no overlap found, can add the new event
+        return True
+'''
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
