d02770345e973b3e20cf22929b21ae11ae21eab7
Henok F <69524541+Henokaa@users.noreply.github.com>
2023-04-09 06:40:52 +0300

    Time: 148 ms (18.46%), Space: 17 MB (5.93%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -3,28 +3,27 @@ class Solution:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         visited = set()
         count = 0
-
+        
+        
         def dfs(i, j):
             visited.add((i, j))
-            is_closed = True
-
             for dx, dy in directions:
                 x, y = i + dx, j + dy
-
                 if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
-                    is_closed = False
+                    self.is_closed = False
+                
                 elif grid[x][y] == 0 and (x, y) not in visited:
-                    if not dfs(x, y):
-                        is_closed = False
+                    dfs(x, y)
                 else:
                     visited.add((x, y))
-
-            return is_closed
+            return
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if (i, j) not in visited and grid[i][j] == 0:
-                    if dfs(i, j):
+                    self.is_closed = True
+                    dfs(i, j)
+                    if self.is_closed == True:
                         count += 1
 
         return count
