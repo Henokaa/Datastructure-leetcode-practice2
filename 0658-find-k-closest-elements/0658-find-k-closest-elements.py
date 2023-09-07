996310bab7680cdc9603aa48cc7ce19d7cad49f3
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-07 09:34:18 -0600

    Time: 257 ms (71.80%), Space: 17.7 MB (94.58%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -2,28 +2,26 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         l = -1
         r = len(arr)
-        value = float('inf')
-        ans = []
         while r > l + 1:
-            mid = l + (r - l)// 2
-            # print(mid, l, r)
+            mid = l + (r - l) // 2
             if arr[mid] > x:
                 r = mid
-            elif arr[mid] <= x:
+            elif arr[mid] < x: 
                 l = mid
-        
-        # print(l, r)
+            else:
+                l = mid
+                
+        # print(l,r)
+                
+        ans = []
         while len(ans) < k:
-            # print(ans)
-            if r > len(arr) - 1 or abs(x - arr[l]) < abs(x - arr[r]) :
+            if r > len(arr) - 1 or abs(x - arr[l]) <= abs(x - arr[r]):
                 ans.append(arr[l])
                 l -= 1
-            elif l < 0 or abs(x - arr[r]) < abs(x - arr[l]):
+            elif l < 0 or abs(x - arr[l]) > abs(x - arr[r]):
                 ans.append(arr[r])
                 r += 1
-            else:
-                ans.append(arr[l])
-                l -= 1
-        # print(ans)
         ans.sort()
         return ans
+            
+                
