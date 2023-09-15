5f08ddd8dbf7e0500d2b0dba961125d8ae97fcad
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-15 06:28:50 -0600

    Time: 609 ms (24.09%), Space: 16.8 MB (96.37%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,55 +1,46 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        # heap
         '''
-        A - 3
-        b - 0
-        c - 0
-        d - 0
-        e - 0
-        f - 0
-        g - 0
-        AbCAdeAfgA _ _ A _ _ A
+        for range of n:
+            pop
+            saved in array
+            or idle
+        add it in saved in heap
         '''
-        #Counter
+        max_heap = []
+        count = 0
+        if n == 0:
+            return len(tasks)
         tasks = Counter(tasks)
-        maxHeap = []
-        for x, y in tasks.items():
-            heapq.heappush(maxHeap, [-y, x])
         
-        # while maxHeap
-        i = 0
-        n = n + 1
-        que = []
-        ans = 0
-        while maxHeap:
-            while i < n:
-                if not maxHeap and not que:
-                    break
-                if maxHeap:
-                    value, char = heapq.heappop(maxHeap)
-                    i += 1
-                    ans += 1
-                    value += 1
-                    if value != 0:
-                        que.append([value, char])
-                else:
-                    ans += 1
-                    i += 1
+        
+        for x,y in tasks.items():
+            heapq.heappush(max_heap, [-y,x])
+        
+        while max_heap:
+            i = n + 1
+            temp = []
             
-            for x, y in que:
-                heapq.heappush(maxHeap, [x, y])
-            que = []
-            i = 0 
-        return ans
+            while i > 0:
+                if max_heap:
+                    num, char = heapq.heappop(max_heap)
+                    if num + 1 < 0:
+                        temp.append([num + 1, char])
+                    count += 1
+                    
+                elif len(temp) > 0:
+                    count += 1
+    
+                i -= 1
+            
+            
+            for x,y in temp:
+                if x < 0: 
+                    heapq.heappush(max_heap, [x,y])
+            # print(max_heap)
                 
+        return count
+            
                 
             
-        
-        # if empty go until n times and increment
-        # array append the poped
-        # if it is 0 don't add
-        
-    
-        
-    
-        
