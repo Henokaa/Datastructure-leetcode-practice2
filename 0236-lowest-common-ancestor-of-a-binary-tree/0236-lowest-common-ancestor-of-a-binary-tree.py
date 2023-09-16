0b0f226359b9da87d56062a394dd906ed54aa72f
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-16 08:52:55 -0600

    Time: 68 ms (56.08%), Space: 28.7 MB (66.19%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,30 +7,33 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        self.ans = None
+        self.temp = None
         def dfs(root):
             if not root:
                 return False
-            
             if root.val == p.val or root.val == q.val:
-                self.ans2 = root
+                self.temp = root
                 return True
+            left = None
+            right = None
             
             left = dfs(root.left)
+                
+            
             right = dfs(root.right)
+                
+            # print(left, right, (root.val))
             
             if left == True and right == True:
                 self.ans = root
             
+            
             return left or right
         
-        self.ans = float('-inf')
-        self.ans2 = 0
         dfs(root)
-        
-        if self.ans == float('-inf'):
-            return self.ans2
-        return self.ans
-        
-        
-             
-                
+        # print(self.ans, self.temp)
+        if self.ans != None:
+            return self.ans
+        if self.temp != None:
+            return self.temp
