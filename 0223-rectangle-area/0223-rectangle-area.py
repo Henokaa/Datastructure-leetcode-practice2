45f2c297e998a1537873a95a1e4b8dffafb8149f
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-16 07:15:16 -0600

    Time: 46 ms (92.38%), Space: 16.2 MB (99.54%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -1,22 +1,20 @@
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        width1 = ax2 - ax1
-        height1 = ay2 - ay1
-        rec1 = height1 * width1
-        width2 = bx2 - bx1
-        height2 = by2 - by1
-        rec2 = width2 * height2
+     
+        x_end = min(bx2, ax2)
+        x_start = max(ax1, bx1)
         
-        x1 = max(ax1, bx1)
-        y1 = max(by1, ay1)
-        x2 = min(ax2,bx2)
-        y2 = min(ay2, by2)
-        
-        x = x2 - x1
-        y = y2 - y1
+        y_start = max(ay1, by1)
+        y_end = min(by2, ay2)
         overlap = 0
-        if x > 0 and y > 0:
-            overlap = x * y
-        # print(rec1, rec2, x, y)
-        ans = rec1 + rec2 - overlap
-        return ans
+        area_1 = (ax2 - ax1) * (ay2 - ay1)
+        area_2 = (bx2 - bx1) * (by2 - by1)
+        if x_end > x_start and y_end > y_start:
+            # print("a")
+            overlap = (x_end - x_start) * (y_end - y_start)
+        
+        # print(overlap)
+        return area_1 + area_2 - overlap
+            
+        
+        
