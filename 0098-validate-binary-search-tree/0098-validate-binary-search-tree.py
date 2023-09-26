2f245a712bd92c174a58f7fc94cc6a17365a0013
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-26 15:56:03 -0600

    Time: 49 ms (50.89%), Space: 18.8 MB (45.35%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,23 +6,14 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-        
         def dfs(left, root, right):
-            
             if not root:
-                    return True
-            
-            if left > root.val > right:
-                r = dfs(left,root.right,root.val)
-                l = dfs(root.val, root.left, right)
-                return l and r
+                return True
+            if left < root.val < right:
+                left = dfs(left, root.left, root.val)
+                right = dfs(root.val, root.right,  right)
+                return left and right
             else:
                 return False
-        
-        if not dfs(float("INF"), root, float("-INF")):
-            return False
-        return True
-'''Brute force- for every node, check if on the left side of the node is less than the root and on thwe right greater o(n^2)
-optimized - o(n)'''
-    
+            
+        return dfs(float("-INF"),root, float("INF"))
