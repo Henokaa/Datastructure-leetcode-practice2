ac80deb2d7939793e8be53bfff75c14a9e8dda04
Henokaa <69524541+Henokaa@users.noreply.github.com>
2023-09-28 07:37:39 -0600

    Time: 721 ms (29.24%), Space: 78.4 MB (53.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,24 +1,59 @@
-from collections import defaultdict, deque
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.cache = defaultdict()
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.order = deque()
+        self.cache = {}
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def get(self, key: int) -> int:
+    def get(self, key):
         if key in self.cache:
-            self.order.remove(key)
-            self.order.append(key)
-            return self.cache[key]
+            node = self.cache[key]
+            self._move_to_front(node)
+            return node.value
         return -1
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key, value):
         if key in self.cache:
-            self.order.remove(key)
-        elif len(self.order) == self.capacity:
-            evicted_key = self.order.popleft()
-            del self.cache[evicted_key]
-        self.order.append(key)
-        self.cache[key] = value
+            node = self.cache[key]
+            node.value = value
+            self._move_to_front(node)
+        else:
+            if len(self.cache) == self.capacity:
+                self._remove_last()
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_front(new_node)
+
+    def _move_to_front(self, node):
+        self._remove_node(node)
+        self._add_to_front(node)
+
+    def _remove_node(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add_to_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_last(self):
+        last_node = self.tail.prev
+        del self.cache[last_node.key]
+        self._remove_node(last_node)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
