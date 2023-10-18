9ba33e9a5cc26ced0d707f8711f079eb88f9d2d9
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2023-10-18 08:09:43 -0600

    Time: 130 ms (5.92%), Space: 22.2 MB (12.17%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,20 +1,28 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
-        def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i == m - 1 and j == n - 1:
-                return grid[i][j]
-            if i == m - 1:
-                memo[(i, j)] = grid[i][j] + dfs(i, j+1)
-            elif j == n - 1:
-                memo[(i, j)] = grid[i][j] + dfs(i+1, j)
-            else:
-                memo[(i, j)] = grid[i][j] + min(dfs(i+1, j), dfs(i, j+1))
-            return memo[(i, j)]
-
-        m = len(grid)
-        n = len(grid[0])
+        directions = [[0,1],[1,0]]
+        inbound = lambda r,c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
         memo = {}
-        return dfs(0, 0)
+        
+        def dfs(x,y):
+            if (x,y) in memo:
+                return memo[(x,y)]
+            # if not inbound(x,y):
+            #     return float('inf')
+            if x == len(grid) - 1 and y == len(grid[0]) - 1:
+                return grid[len(grid) - 1][len(grid[0]) - 1]
+            
+            left = float('inf')
+            down = float('inf')
+            if inbound(x+1, y):
+                left = dfs(x + 1, y)
+            if inbound(x, y + 1):
+                down = dfs(x, y + 1)
+            # print(x,y)
+            res = min(left, down) + grid[x][y]
+            memo[(x,y)] = res
+            return memo[(x,y)]
+        
+        
+        return dfs(0,0)
+                
