bbf7d45c4d70c1dd7431a43405c4b20032cad8f4
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2023-10-24 05:40:19 -0600

    Time: 257 ms (59.57%), Space: 17.6 MB (99.56%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -2,26 +2,35 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         l = -1
         r = len(arr)
+        index
         while r > l + 1:
             mid = l + (r - l) // 2
-            if arr[mid] > x:
+            if arr[mid] ==x:
                 r = mid
-            elif arr[mid] < x: 
-                l = mid
+            elif arr[mid] > x:
+                r = mid
             else:
                 l = mid
-                
-        # print(l,r)
-                
+        print(l,r)
+        
         ans = []
-        while len(ans) < k:
-            if r > len(arr) - 1 or abs(x - arr[l]) <= abs(x - arr[r]):
-                ans.append(arr[l])
-                l -= 1
-            elif l < 0 or abs(x - arr[l]) > abs(x - arr[r]):
+        while k > 0:
+            if r < len(arr) and l > -1 and abs(arr[r] - x) < abs(arr[l] - x):
                 ans.append(arr[r])
                 r += 1
+                k -= 1
+            elif k > 0 and l > -1 and r < len(arr) and abs(arr[r] - x) >= abs(arr[l] - x):
+                ans.append(arr[l])
+                l -= 1
+                k -= 1
+            elif l > -1 and r >= len(arr):
+                ans.append(arr[l])
+                l -= 1
+                k -= 1
+            elif r < len(arr) and l <= -1:
+                ans.append(arr[r])
+                r += 1
+                k -= 1
+                
         ans.sort()
         return ans
-            
-                
