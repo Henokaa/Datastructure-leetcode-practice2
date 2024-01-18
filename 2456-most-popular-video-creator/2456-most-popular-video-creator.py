5864ab34f8850ee4683432e729659afc66a84c0a
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-01-18 08:54:48 -0700

    Time: 675 ms (86.39%), Space: 60.6 MB (96.86%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.py
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.py
@@ -1,30 +1,46 @@
 class Solution:
     def mostPopularCreator(self, creators: List[str], ids: List[str], views: List[int]) -> List[List[str]]:
         '''
-        alice : [(one,5) 
+        x,y,z
         
+        alice = one, 5
+        bob = two, 5
+        
+        alice = 10
+        bob = 10
+        
+        alice = 
         '''
-        total = defaultdict(int)
-        saved = {}
-        mx = float('-inf')
-        for creator, i, view in zip(creators, ids, views):
-            total[creator] += view
-            if creator not in saved:
-                saved[creator] = (i, view)
+        
+        name = {}
+        values = {}
+        maximum = 0
+        for creators, ids, views in zip(creators, ids, views):
+            if creators in name:
+                if views >= name[creators][1]:
+                    if views == name[creators][1]:
+                        if name[creators][0] > ids:
+                            name[creators] = (ids, views)
+                    else:
+                        name[creators] = (ids, views)
             else:
-                if view > saved[creator][1]:
-                    saved[creator] = (i, view)
-                elif view == saved[creator][1] and i < saved[creator][0]:
-                    saved[creator] = (i, view)
-                    
-        mx = max(total.values())
-        ans = []
-        for x in total.keys():
-            if total[x] == mx:
-                ans.append([x,saved[x][0]])
+                name[creators] = (ids, views)
                 
-        return ans
+            if creators in values:
+                values[creators] += views
+                maximum = max(maximum, values[creators])
+            else:
+                values[creators] = views
+                maximum = max(maximum, values[creators])
+        
+        answer = []
+        for key, result in values.items():
+            if result == maximum:
+                answer.append([key ,name[key][0]])
+            
+        return answer
+        
         
             
-                    
+            
             
