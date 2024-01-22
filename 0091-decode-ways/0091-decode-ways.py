cd81b4dc7d175ca97e2ae9e4c8840d553addb6c0
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-01-22 09:40:38 -0700

    Time: 41 ms (35.68%), Space: 16.6 MB (88.11%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @cache
-        def dfs(i):
-            if i > len(s) - 1:
-                return 1
-            # print(i)
-            
-            first = s[i:i+1]
-            l = 0
-            if len(first) == 1 and first[0] != "0" and 1 <= int(first) <= 26:
-                l = dfs(i + 1)
-            sec = s[i:i+2]
-            r = 0
-            if len(sec) == 2 and sec[0] != "0" and 1 <= int(sec) <= 26:
-                r = dfs(i + 2)
-            
-            return l + r
+        n = len(s)
+        if n == 0 or s[0] == '0':  
+            return 0
+
         
-        return dfs(0)
+        dp = [0] * (n + 1)
+        dp[n] = 1  
+
+        for i in range(n - 1, -1, -1):
+            if s[i] != '0':
+                dp[i] += dp[i+1]  
+
+            if i + 1 < n and (s[i] == '1' or (s[i] == '2' and s[i+1] <= '6')):
+                dp[i] += dp[i+2]  
+                
+        print(dp)
+        return dp[0]
