86d06bab2aafb5a7a13f06af3e39e372428900eb
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-01-22 12:49:28 -0700

    Time: 39 ms (47.72%), Space: 16.6 MB (55.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        temp = 0
-        dp = []
-        for i in range(len(nums)):
-            if i < 2:
-                temp = max(temp, nums[i])
-                dp.append(temp)
-                continue
-            temp = 0
-            temp = max(dp[i-2] + nums[i], dp[i-1])
-            dp.append(temp)
-        return dp[-1]
+        n = len(nums)
+        memo = {}  # Memoization dictionary
+
+        def dp(i):
+            if i in memo:
+                return memo[i]
+
+            if i >= n:
+                return 0
+
+           
+            rob_current = nums[i] + dp(i + 2)
+
             
-        
-        
+            skip_current = dp(i + 1)
+
+            # Store the maximum amount in the memoization dictionary
+            memo[i] = max(rob_current, skip_current)
+
+            return memo[i]
+        left = dp(0)
+        print(memo)
+        return left
