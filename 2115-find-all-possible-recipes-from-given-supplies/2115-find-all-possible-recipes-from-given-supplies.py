0160db32c8e1a40b7dfde5c15648f1d704e17275
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-02-05 06:38:25 -0700

    Time: 844 ms (49.48%), Space: 19.8 MB (72.52%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,64 +1,32 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        checkrec = set(recipes)
-        checksup = set(supplies)
-        indegree = {}
-        # print(checkrec)
+        '''
+        que = [supplies]
+        
+        graph["yeast"] = "bread"
+        indegree["bread"] += 1
+        
+        '''
         graph = defaultdict(list)
-        for i in range(len(recipes)):
-            indegree[i] = 0
+        indegree = defaultdict(int)
+        que = deque()
         for i in range(len(ingredients)):
             for j in range(len(ingredients[i])):
-                if ingredients[i][j] in checkrec:
-                    index = recipes.index(ingredients[i][j])
-                    graph[index].append(i)
-                    indegree[i] += 1
-        
-        # print(graph)
-        # print(indegree)
-        que = deque()
-        visited = set()
-        valid = set()
-        for x,y in indegree.items():
-            if y == 0:
-                flag = 0
-                for item in ingredients[x]:
-                    if item not in checksup:
-                        flag = 1
-                if flag == 0:
-                    que.append(x)
-                    visited.add(x)
-                    valid.add(recipes[x])
-
-        ans = set()
-        # print(que)
+                graph[ingredients[i][j]].append(recipes[i])
+                indegree[recipes[i]] += 1
+            
+        for x in supplies:
+            que.append(x)
+        answer = []
         while que:
-            food = que.popleft()
-            flag = 0
-            for x in ingredients[food]:
-                if x in checksup or x in valid:
-                    continue
-                else:
-                    flag = 1
-            if flag == 0:
-                ans.add(recipes[food])
-                valid.add(recipes[food])
-                for child in graph[food]:
-                    if child not in visited:
-                        que.append(child)
-                        # valid.add(recipes[child])
-                        visited.add(child)
-                        
-
-#         for i in range(len(recipes)):
-#             if recipes[i] not in ans:
-#                 flag = 0
-#                 for x in range(len(ingredients[i])):
-#                     if ingredients[i][x] not in checksup or ingredients[i][x] not in checkrec:
-#                         flag = 1
-                        
-#                 if flag == 0:
-#                     ans.append(recipes[i])
-        return list(ans)
+            output = que.popleft()
+            for child in graph[output]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    que.append(child)
+                    answer.append(child)
                 
+        return answer
                     
+                
+            
