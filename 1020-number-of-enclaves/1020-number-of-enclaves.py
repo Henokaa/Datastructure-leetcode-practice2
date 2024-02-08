13063d7b80fc443a7b3e28efbf2c398592514e8f
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-02-08 07:38:28 -0700

    Time: 508 ms (58.46%), Space: 39.8 MB (34.66%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,38 +1,52 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        self.count = 0
+        '''
+        cover row and cols
+        do dfs(1) save in visted
+        '''
         visited = set()
-        directions = [[1,0], [0,1], [-1,0], [0,-1]]
-        inbound = lambda r,c: 0<= r <len(grid) and 0<= c <len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    self.count += 1
-                    
+        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        
+        
         def dfs(i,j):
             if (i,j) in visited:
-                 return 
-            
-            self.count -= 1
+                return
             visited.add((i,j))
-            for x,y in directions:
-                dx = i + x
-                dy = j + y
-                if inbound(dx,dy) and grid[dx][dy] == 1 and (dx, dy) not in visited:
-                    dfs(dx,dy)
-          
-        for i in range(len(grid[0])):
-            if grid[0][i] == 1:
-                dfs(0,i)
-            if grid[len(grid) - 1][i] == 1:
-                dfs(len(grid)-1, i)
+            for x, y in directions:
+                dr = i + x
+                dc = j + y
+                if inbound(dr,dc) and grid[dr][dc] == 1:
+                    dfs(dr,dc)
+            
+        visited = set()
         
-        for j in range(len(grid)):
-
-            if grid[j][0] == 1:
-                dfs(j,0)
-            if grid[j][len(grid[0]) - 1] == 1:  # this got me bad
-                dfs(j, len(grid[0]) - 1)
-        print(visited)
-        return self.count
+        for r in range(len(grid)):
+            if grid[r][0] == 1:
+                if (r,0) not in visited:
+                    dfs(r,0)
+                
+            if grid[r][len(grid[0])-1] == 1:
+                if (r,len(grid[0])-1) not in visited:
+                    dfs(r, len(grid[0])-1)
+                
+                
+                
+        for c in range(len(grid[0])):
+            if grid[0][c] == 1:
+                if (0,c) not in visited:
+                    dfs(0,c)
+            
+            if grid[len(grid)-1][c] == 1:
+                if (len(grid)-1,c) not in visited:
+                    dfs(len(grid)-1,c)
         
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    count += 1
+                    
+        return count
+                    
+            
