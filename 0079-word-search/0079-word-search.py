4ff6629be7e52f7eae3f50f94b69906f5cff61be
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-02-10 23:20:22 -0700

    Time: 5345 ms (33.53%), Space: 16.5 MB (78.48%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,19 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        '''
+        
+        '''
+        directions = [[0,1], [1,0], [-1,0], [0,-1]]
+        inbound = lambda x, y: 0 <= x < len(board) and 0 <= y < len(board[0])
+        def dfs(i,j,k):
+            if k == len(word):
+                return True
+            
+            
+            
+            for x,y in directions:
+                dr = x + i
+                dc = y + j
+                
+                if inbound(dr,dc) and board[dr][dc] == word[k] and (dr,dc) not in visited:
+                    visited.add((dr,dc))
+                    if dfs(dr,dc,k+1):
+                        return True
+                    visited.remove((dr,dc))
+                    
+                
+        
         for i in range(len(board)):
             for j in range(len(board[i])):
-                if self.backtracking(i, j,word,board):
-                    return True
-        return False
-    def backtracking(self,i, j,word,board):
-            if len(word) == 0:
-                return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[i]):
-                return False
-            if board[i][j] == word[0]:
-                board[i][j] = "~"
-                if self.backtracking(i+1, j, word[1:],board) or self.backtracking(i-1, j, word[1:],board) or self.backtracking(i, j+1, word[1:],board) or self.backtracking( i, j-1, word[1:],board):
-                    return True
-                board[i][j] = word[0]
-            return False
+                
+                if board[i][j] == word[0]:
+                    visited = set()
+                    visited.add((i,j))
+                    if dfs(i,j, 1):
+                        return True
         
+        return False
