5813d519b6d583fa0774983c2b4b8139612f5a17
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-03-06 06:45:32 -0700

    Time: 582 ms (14.63%), Space: 18.1 MB (43.58%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,45 +1,41 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        '''
-        bfs 
-        
-        '''
-        
-        # declare our direction
-        directions = [[0,1],[1,0],[-1, 0], [0, -1],[1,1], [-1,-1],[-1,1],[1,-1]]
-        inbound = lambda r, c: 0<=r<len(grid) and 0 <= c<len(grid[0])
-        # que
         que = deque()
-        n = len(grid)
-        if grid[0][0] or grid[n-1][n-1]:
-            return -1
-        if len(grid) == 1 and grid[0][0] == 0:
-            return 1
-        if grid[0][0] == 0:
-            que.append((0,0))
-        else:
-            return -1
-    
+        directions = [(0,1), (1,0), (-1,0), (0,-1), (-1,1),(-1,-1),(1,1),(1,-1)]
+        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
         visited = set()
-        visited.add((0,0))
-        level = 1
-        # bfs 
+        if grid[0][0] != 0:
+            return -1
         
+        if grid[0][0] == 0 and 0 == len(grid)-1 and 0 == len(grid[0]) - 1:
+            return 1
+        
+        que.append((0,0))
+        
+        level = 1
         while que:
-            # print(que)
             length = len(que)
+            # print(que, level)
             for i in range(length):
-                temp1 = que.popleft()
+                position = que.popleft()
                 for x,y in directions:
-                    dr = temp1[0] + x
-                    dc = temp1[1] + y
-                    if inbound(dr,dc) and (dr,dc) not in visited and grid[dr][dc] == 0:
-                        
-                        if (dr,dc) == (len(grid)-1,len(grid[0])-1):
-                            return level + 1
-                        que.append((dr,dc))
-                        visited.add((dr,dc))
+                    x1 = position[0] + x
+                    y1 = position[1] + y
+                    
+                    if x1 == len(grid) - 1 and y1 == len(grid[0]) - 1 and grid[x1][y1] == 0:
+                        # print("A",(x1, y1), (position))
+                        return level + 1
+                    
+                    if inbound(x1,y1) and (x1,y1) not in visited and grid[x1][y1] == 0:
+                        visited.add((x1,y1))
+                        que.append((x1,y1))
             
             level += 1
+        
         return -1
+                    
+                
+            
+            
+            
                 
