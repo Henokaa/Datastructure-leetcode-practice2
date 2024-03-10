10d0bf03f9b34aadae5c89fa8785a5e7b85beb17
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-03-09 20:35:54 -0700

    Time: 60 ms (14.39%), Space: 21 MB (58.27%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,44 +8,31 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         '''
-        if l == True and r == True
-        
-        if root.val == p and (l ==True or r == True)
-        
-        if root.val == q and (l ==True or r == True)
-        
-        if root.val == p.val:
-            return true
-        
+        l & r == True
+        root.value == p and l or r True
         
         '''
         self.answer = None
-        self.p_val = None
-        self.q_val = None
         def dfs(root):
             if not root:
                 return False
- 
+            
             left = dfs(root.left)
             right = dfs(root.right)
-            
+            # print(root.val, left, right)
             if left == True and right == True:
                 self.answer = root
                 return False
             
-            if root.val == p.val and (left == True or right == True):
-                self.answer = root
-                return False
-            
-            if root.val == q.val and (left == True or right == True):
-                self.answer = root
-                return False
+            if root.val == p.val or root.val == q.val:
+                if left == True or right == True:
+                    self.answer = root
+                    return False
             
             if root.val == p.val or root.val == q.val:
-                # print("A",root.val)
                 return True
-            
             return left or right
+            
         
         dfs(root)
         return self.answer
