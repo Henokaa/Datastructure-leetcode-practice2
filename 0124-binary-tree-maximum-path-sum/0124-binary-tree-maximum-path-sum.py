74f62536bd8389a287c41d94f2ac8fd2c1311ddc
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-03-09 22:29:18 -0700

    Time: 67 ms (42.12%), Space: 21.1 MB (22.24%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,27 +6,42 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        '''
+        left
+        right
+        
+        total = root.val
+        if left > 0
+            
+        if right > 0
+        
+        
+        return max(left, right)
+        '''
+        self.answer = float('-inf')
         def dfs(root):
             if not root:
                 return 0
+            
             left = dfs(root.left)
             right = dfs(root.right)
-            mx = max(left, right)
             
-            if left >= 0 and right >= 0:
-                # print(left, right, root.val)
-                self.ans = max(self.ans, left + right + root.val)
-            elif left <= 0 and right >= 0:
-                self.ans = max(self.ans, right + root.val)
-            elif left >= 0 and right <= 0:
-                self.ans = max(self.ans, left + root.val)
-            elif left <= 0 and right <= 0:
-                self.ans = max(self.ans, root.val)
-                
-            if mx < 0:
-                mx = 0
-            return mx + root.val
-
-        self.ans = float('-inf')
-        temp = dfs(root) 
-        return self.ans
+            total = root.val
+            total_left = 0
+            total_right = 0
+            
+            if left > 0:
+                total += left
+                total_left += left
+            
+            if right > 0:
+                total += right
+                total_right += right
+            
+            self.answer = max(self.answer, total)
+            temp = max(total_left+root.val, total_right+root.val)
+            
+            return temp
+            
+        dfs(root)
+        return self.answer
