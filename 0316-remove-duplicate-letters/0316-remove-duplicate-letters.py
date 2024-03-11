da5508a8a5dbc01f116072f7b7e243094bccf33c
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-03-11 07:19:00 -0600

    Time: 47 ms (15.40%), Space: 16.6 MB (56.97%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,18 +1,46 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
+        '''
+        bcabc
+        bc
+        
+        last_dict
+        if last_dict >
+            pop
+        '''
+        saved_letter = {}
+        for i in range(len(s)):
+            saved_letter[s[i]] = i
+        
+        # print(saved_letter)
+        
+        r = 0
         stack = []
-        seen = set()
-        last_occurrence = {c: i for i, c in enumerate(s)}
-        # print(last_occurrence)
-        for i, c in enumerate(s):
-            if c not in seen:
-                while stack and c < stack[-1] and i < last_occurrence[stack[-1]]:
-                    seen.remove(stack.pop())
-                stack.append(c)
-                seen.add(c)
-            # print(stack, seen)
-
-        return ''.join(stack)
-    '''
-    bca  -> a removed every one
-    '''
+        visited = set()
+        while r < len(s):
+            # print(stack)
+            if s[r] in visited:
+                r += 1
+                continue
+            if not stack:
+                stack.append(s[r])
+                visited.add(s[r])
+            else:
+                if stack[-1] <= s[r]:
+                    if s[r] not in visited:
+                        stack.append(s[r])
+                        visited.add(s[r])
+                else:
+                    while stack and saved_letter[stack[-1]] > r and stack[-1] > s[r]:
+                        last = stack[-1]
+                        stack.pop()
+                        visited.remove(last)
+                    if s[r] not in visited:
+                        stack.append(s[r])
+                        visited.add(s[r])
+            r += 1
+            
+            
+        return "".join(stack)
+            
+        
