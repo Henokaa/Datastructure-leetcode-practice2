dc24143e41933f6e2c201b279695320a9e4615af
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-03-16 11:33:10 -0600

    Time: 75 ms (52.38%), Space: 18.3 MB (51.62%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,16 +1,33 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        result = []
-        dq = deque([(0, [0])])
-        target = len(graph) - 1
-        while dq:
-            # print(dq)
-            cur,route = dq.popleft()
-            if cur == target:
-                result.append(route)
-            else:
-                for node in graph[cur]:
-                    route.append(node)
-                    dq.append((node, route.copy()))
-                    route.pop()
-        return result
+        '''
+        bulf graph
+         dfs(i, visited, stack)
+        '''
+        end = len(graph) - 1
+        answer = []
+        graph_2 = defaultdict(list)
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                graph_2[i].append(graph[i][j])
+        
+        
+            
+        def dfs(i, stack, visited):
+
+            for child in graph[i]:
+                if child == end:
+                    stack2 = stack.copy()
+                    stack2.append(child)
+                    answer.append(stack2)
+                elif child not in visited:
+                    stack.append(child)
+                    visited.add(child)
+                    dfs(child, stack, visited)
+                    visited.remove(child)
+                    stack.pop()
+                
+        
+        dfs(0, [0], set([0]))
+        return answer
+        
