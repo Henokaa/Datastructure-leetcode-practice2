831c45fb20946c6fd53492f79632ca040d4a0f71
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-04-24 22:02:23 -0600

    Time: 5544 ms (15.52%), Space: 16.5 MB (95.23%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,7 +1,7 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         '''
-        The key thing is you need to backtrack in the visited set, so that it can be reached from         another path
+        
         '''
         directions = [[0,1], [1,0], [-1,0], [0,-1]]
         inbound = lambda x, y: 0 <= x < len(board) and 0 <= y < len(board[0])
@@ -9,19 +9,21 @@ class Solution:
             if k == len(word):
                 return True
             
-            
-            
+            # print(k)
+            node = False
             for x,y in directions:
                 dr = x + i
                 dc = y + j
-                
                 if inbound(dr,dc) and board[dr][dc] == word[k] and (dr,dc) not in visited:
-                    visited.add((dr,dc))
-                    if dfs(dr,dc,k+1):
+                    visited.add((i,j))
+                    node =  dfs(dr,dc,k+1)
+                    if node == True:
                         return True
-                    visited.remove((dr,dc))
-                    
+                    visited.remove((i,j))
+            
+            return False
                 
+            
         
         for i in range(len(board)):
             for j in range(len(board[i])):
@@ -31,5 +33,5 @@ class Solution:
                     visited.add((i,j))
                     if dfs(i,j, 1):
                         return True
-        
+                    
         return False
