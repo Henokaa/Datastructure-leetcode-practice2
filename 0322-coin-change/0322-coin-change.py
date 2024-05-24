4ad54bb76439cc8717b8e333f24519a655f36700
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-05-23 22:27:42 -0600

    Time: 1266 ms (10.87%), Space: 49.7 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,45 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         '''
+        dfs(i):
+            coins[i] + dfs(i)
+            dfs(i+1)
+        '''
+        '''
+        dfs(cur):
+            small
+            for i
+                if cur + i < amount
+                    temp = 1 + dfs(cur + i)
+                min(small, )
+        '''
+        
+        '''
+        dp(total)
+        # memoize - amount
+        # route - return min
         
         '''
         saved = {}
-        def dp(num):
-            if num in saved:
-                return saved[num]
-            if num == amount:
+        def dp(total):
+            
+            if total == amount:
                 return 0
             
-            shortest = float("inf")
+            if total in saved:
+                return saved[total]
+            
+            minimum = float('inf')
             for i in range(len(coins)):
-                if coins[i] + num <= amount:
-                    total = 1 + dp(coins[i] + num)
-                    shortest = min(shortest, total)
-                    
-            saved[num] = shortest
-            return shortest
-
-        ans = dp(0)
-        if ans == float("inf"):
+                if coins[i] + total <= amount:
+                    path = 1 + dp(total + coins[i])
+                    minimum = min(minimum ,path)
+            
+            saved[total] = minimum
+            return minimum
+            
+            
+        res = dp(0)
+        if res == float('inf'):
             return -1
-        return ans
-        
+        return res
