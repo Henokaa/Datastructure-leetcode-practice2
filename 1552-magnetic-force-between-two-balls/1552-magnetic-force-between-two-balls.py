c2f216578d3f55a906122c12940de44a4ade7fc1
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-07-15 21:09:52 -0600

    Time: 896 ms (39.83%), Space: 30.4 MB (17.40%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -1,32 +1,42 @@
 class Solution:
     def maxDistance(self, position: List[int], m: int) -> int:
         '''
-        _, 
-        '''
-        # Sort the positions
-        position.sort()
+        sort
+        start and end
+        if possible -> mid
+            l = mid
         
-        def isPossible(x):
-            count = 1
-            prev = position[0]
-            for p in position:
-                if p - prev >= x:
-                    prev = p
+        
+        
+        ---
+        6
+        
+        '''
+        position.sort()
+        # print(position)
+        l = 1
+        r = position[-1] - position[0]
+        
+        def valid(mid):
+            count = 0
+            prev = float('-inf')
+            for i in range(len(position)):
+                if position[i] >= prev + mid:
+                    prev = position[i]
                     count += 1
+                    
             if count >= m:
                 return True
             return False
-        
-        l = 1
-        r = position[-1] - position[0]
-        # print(position, l, r)
+                
         while l <= r:
             mid = l + (r - l) // 2
-            if isPossible(mid):
+            # print(mid)
+            if valid(mid):
                 l = mid + 1
-                answer = mid
+                ans = mid
             else:
                 r = mid - 1
-        
-        return answer
-        
+                
+        return ans
+            
