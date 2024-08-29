3054ae25ef3bb041c6cdedc46d533f91202f7ab4
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-08-29 07:23:09 -0600

    Time: 546 ms (19.98%), Space: 18.3 MB (14.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,41 +1,31 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        '''
+        node
+        bfs
+        que(x,y, level)
+        8 direct
+        '''
+        directions = [(0,1), (1,0), (-1,0), (0,-1), (-1,-1), (1,1), (-1,1), (1,-1)]
+        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0]) 
         que = deque()
-        directions = [(0,1), (1,0), (-1,0), (0,-1), (-1,1),(-1,-1),(1,1),(1,-1)]
-        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0])
-        visited = set()
-        if grid[0][0] != 0:
+        if not grid[0][0] == 0:
             return -1
-        
-        if grid[0][0] == 0 and 0 == len(grid)-1 and 0 == len(grid[0]) - 1:
-            return 1
-        
-        que.append((0,0))
-        
-        level = 1
+        visited = set()
+        que.append((0,0,0))
+        visited.add((0,0))
         while que:
-            length = len(que)
-            # print(que, level)
-            for i in range(length):
-                position = que.popleft()
-                for x,y in directions:
-                    x1 = position[0] + x
-                    y1 = position[1] + y
-                    
-                    if x1 == len(grid) - 1 and y1 == len(grid[0]) - 1 and grid[x1][y1] == 0:
-                        # print("A",(x1, y1), (position))
-                        return level + 1
-                    
-                    if inbound(x1,y1) and (x1,y1) not in visited and grid[x1][y1] == 0:
-                        visited.add((x1,y1))
-                        que.append((x1,y1))
+            x,y, level = que.popleft()
+            # print((x,y))
+            if x == len(grid)-1 and y == len(grid[0])-1:
+                return level +1
             
-            level += 1
+            for x_1, y_1 in directions:
+                if inbound(x+x_1, y+y_1) and grid[x+x_1][y+y_1] == 0:
+                    if (x +x_1, y+y_1) in visited:
+                        continue
+                    que.append((x+x_1, y+y_1, level + 1))
+                    visited.add((x+x_1, y+y_1))
         
         return -1
-                    
-                
             
-            
-            
-                
