91914f539fe2dcb71d67f66df7ad5bd899327e87
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-09-19 09:56:11 -0600

    Time: 435 ms (62.01%), Space: 18.3 MB (5.60%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,31 +1,36 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         '''
-        node
         bfs
-        que(x,y, level)
-        8 direct
         '''
-        directions = [(0,1), (1,0), (-1,0), (0,-1), (-1,-1), (1,1), (-1,1), (1,-1)]
-        inbound = lambda x,y: 0 <= x < len(grid) and 0 <= y < len(grid[0]) 
-        que = deque()
-        if not grid[0][0] == 0:
-            return -1
-        visited = set()
-        que.append((0,0,0))
-        visited.add((0,0))
-        while que:
-            x,y, level = que.popleft()
-            # print((x,y))
-            if x == len(grid)-1 and y == len(grid[0])-1:
-                return level +1
+        #
+        directions = [(0,1), (1,0), (-1,0), (0, -1), (1,1), (1,-1), (-1, 1), (-1,-1)]
+        inbound = lambda x,y: 0<= x < len(grid) and 0<= y < len(grid[0])
             
-            for x_1, y_1 in directions:
-                if inbound(x+x_1, y+y_1) and grid[x+x_1][y+y_1] == 0:
-                    if (x +x_1, y+y_1) in visited:
-                        continue
-                    que.append((x+x_1, y+y_1, level + 1))
-                    visited.add((x+x_1, y+y_1))
+        if grid[0][0] == 1:
+            return -1
+        
+        que = deque()
+        visited = set()
+        que.append((0,0))
+        visited.add((0,0))
+        level = 0
+        while que:
+            length = len(que)
+            for _ in range(length):
+                i,j = que.popleft()
+
+                if len(grid) - 1 == i and len(grid[0]) - 1 == j and grid[i][j] == 0:
+                    return level + 1
+
+                for x,y in directions:
+                    x_1 = x + i
+                    y_1 = y + j
+
+                    if (x_1, y_1) not in visited and inbound(x_1, y_1) and grid[x_1][y_1] == 0:
+                        que.append((x_1, y_1))
+                        visited.add((x_1, y_1))
+
+            level += 1
         
         return -1
-            
