5f00f2ef82fac02f225d4a6faf8e11e604304fc0
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-09-18 19:49:51 -0600

    Time: 327 ms (95.49%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,13 +1,11 @@
 # Write your MySQL query statement below
-# there is sbuquery solution, largest single number, or null
-SELECT IFNULL((
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-    ORDER BY num DESC
-    LIMIT 1
-), NULL) AS num;
+# largest single number, single, large
 
-
+select max(unique_number.num) as num
+from (
+    select num
+    from mynumbers
+    Group by num
+    having count(num) = 1
+) as unique_number
 
