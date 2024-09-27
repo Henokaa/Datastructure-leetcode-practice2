e0ee6b9b7683046f0468eebf8f8a76a219618e49
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-09-27 05:05:56 -0600

    Time: 67 ms (84.97%), Space: 17.2 MB (49.77%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,23 +1,27 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        if not board:
-            return 0
-
+        '''
+        check ert, hor
+        
+        o (r * c)
+        o (r * c)
+        '''
+        
+        '''
+        if x
+            check prev (horz, vert), 
+            
+        '''
         count = 0
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == 'X':
-                    if i > 0 and board[i-1][j] == 'X':
-                        continue
-                    if j > 0 and board[i][j-1] == 'X':
-                        continue
-                    count += 1
-        return count
+                
+                if board[i][j] == "X":
+                    
+                    if (i == 0 or board[i-1][j] != "X") and (j == 0 or board[i][j-1] != "X"):
+                        count += 1
                         
-        # horizontal
-        # n + 1
-        # it should be the start
-        
-        # vertical
-        # one down
-        # it should be the start
+            # print(count)
+
+        return count
+                    
