ffbe6c33056d3b19854f6009515bd8060500d65e
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-10-12 05:27:32 -0600

    Time: 37 ms (62.65%), Space: 16.4 MB (86.36%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
@@ -7,27 +7,27 @@
 class Solution:
     def distributeCoins(self, root: Optional[TreeNode]) -> int:
         '''
-        Key Observations:
-Total Coins and Nodes Match: Since the total number of coins equals the total number of nodes, the goal is to distribute the coins such that each node ends up with exactly one coin.
-Excess and Deficit:
-A node with more than one coin (excess) needs to give away coins.
-A node with zero coins (deficit) needs to receive coins.
-Net Balance: The net balance of coins for a node is defined as number of coins - 1. This tells us whether a node has excess coins (positive balance) or needs coins (negative balance).
+        left
+        right
+        
+        return coin left
+        move
+        
+        return abs(left) + abs(right) - 1 
         '''
-        def dfs(node):
-            if not node:
+        self.move = 0
+        def dfs(root):
+            if not root:
                 return 0
             
-            left_balance = dfs(node.left)
-            right_balance = dfs(node.right)
+            left = dfs(root.left)
+            right = dfs(root.right)
             
-            # Total moves is the sum of absolute balances from left and right
-            moves[0] += abs(left_balance) + abs(right_balance)
+            self.move += abs(left) + abs(right)
             
-            # Balance of this node
-            return node.val + left_balance + right_balance - 1
-
-        moves = [0]
-        dfs(root)
-        return moves[0]
+            coins = root.val + left + right - 1 
+            
+            return coins
         
+        dfs(root)
+        return self.move
