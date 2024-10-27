193fb9eac16ab5d32b7ae06d4b0f2a73f690eab1
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-10-26 18:00:27 -0600

    Time: 3 ms (54.73%), Space: 17.8 MB (17.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,32 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ''' 
-        [3,2,4]
-        target = 6
-        
-        [2, 3, 4]   => 6
-         ^
-            ^
-            
-        [3,2,4]
-        
-        
-        
-        [3, 3]
-        
-        3:1
-        
-        
         '''
-        hashmap = {}
+        understand
         
+        
+        constraint
+        
+        naive approach
+        [2,7,11,15]  = 9
+           ^
+        iterate
+            visted,add
+            check
+        
+        optimal 
+        
+        edge
+    
+        '''
+        visited = {}
         for i in range(len(nums)):
-            number = target - nums[i]
-            if number in hashmap:
-                return [i, hashmap[number]]
             
-            hashmap[nums[i]] = i
+            number = target - nums[i]
+            if number in visited:
+                return [visited[number], i]
+            visited[nums[i]] = i
         
-        return []
+            
         
         
