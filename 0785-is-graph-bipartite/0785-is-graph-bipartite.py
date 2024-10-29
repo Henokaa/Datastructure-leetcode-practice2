0e9902b6fe34ed268e2a210064f3f582f1dcd3b3
Henok Tadele <69524541+Henokaa@users.noreply.github.com>
2024-10-29 13:41:39 -0600

    Time: 90 ms (5.13%), Space: 18 MB (5.70%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,41 +1,43 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         '''
-        x = popleft()
-        parent blue1
-        
-        children green2
-        
-        if parent is blue the children must be green
-        it must never be the same color
-        '''
-        node = {}
-        for i in range(len(graph)):
-            node[i] = graph[i]
-        
-        
-        que = deque()
-        visited1 = {}
-        for i in range(len(graph)):
-            if i in visited1:
-                continue
-            visited1[i] = 0
-            que = deque()
-            que.append((i, 0))
-            while que:
-                parent = que.popleft()
-                
-                for x in graph[parent[0]]:
-                    if x not in visited1:
-                        if parent[1] == 1:
-                            visited1[x] = 0
-                            que.append((x, 0))
-                        else:
-                            visited1[x] = 1
-                            que.append((x, 1))
-                    else:
-                        if visited1[x] == parent[1]:
-                            return False
-                        
-        return True
+        constraint
             
+        visted[node] = color
+        for 
+            if not visited
+                que.append (node, 0)
+                    while que
+                        node
+                            for child 
+                                if child in visited:
+                                    visited[child] == color
+                                        return False
+                                    
+        
+        edge case
+        
+        '''
+        visited = {}
+        que = deque()
+        for elements in range(len(graph)):
+            if elements not in visited:
+                color = 0
+                que.append((elements, color))
+                while que:
+                    node, new_color = que.popleft()
+                    visited[node] = new_color
+                    for child in graph[node]:
+                        if child in visited:
+                            if visited[child] == new_color:
+                                return False
+                        else:
+                            if new_color == 1:
+                                que.append((child, 0))
+                    
+                            else:
+                                que.append((child, 1))
+        
+        return True
+                            
+        
